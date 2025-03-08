99a60c38900c9c098f9d0059590de73f4832c330
ticlekiwi <florian.nicolas@yahoo.fr>
2025-03-08 09:56:13 +0100

    fixing tracking on hero section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
       <h1 className="mx-auto text-balance lg:max-w-3xl text-[2.5rem] leading-[2.75rem] lg:text-[5rem] lg:leading-[5.5rem] tracking-[-.02em] font-medium">
         Vos&nbsp;établissements méritent une plateforme à leur mesure.
       </h1>
-      <div className="mt-6 max-w-lg mx-auto text-[1rem] leading-[1.5rem] lg:text-[1.5rem] lg:leading-[2rem] tracking[-.01em] font-medium bg-gradient-to-r from-[#3d77d3] to-[#9b59c4] bg-clip-text text-transparent">
+      <div className="mt-6 max-w-lg mx-auto text-[1rem] leading-[1.5rem] lg:text-[1.5rem] lg:leading-[2rem] tracking-[-.01em] font-medium bg-gradient-to-r from-[#3d77d3] to-[#9b59c4] bg-clip-text text-transparent">
         <p>De l&apos;acquisition à la satisfation client :</p>
         <p>L&apos;audace comme signature,</p>
         <p className="inline-flex items-center">
