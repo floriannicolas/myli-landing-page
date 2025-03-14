2872d21a1eebde3eac786f6d45d9decc01ebd2b7
ticlekiwi <florian.nicolas@yahoo.fr>
2025-03-14 18:20:23 +0100

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
         <footer className="py-6 px-4 mt-4 lg:py-14 lg:px-20 bg-[#f5f6f8] rounded-t-[2rem]">
           <div className="flex flex-col lg:flex-row items-start justify-between">
             <div className="flex lg:flex-col items-center justify-between lg:justify-start gap-6 w-full lg:w-auto">
-              <Link href="/" aria-label="Myli">
+              <Link href="/" aria-label="Myli" className="overflow-hidden w-26 hover:w-auto">
                 <LogoMyliAnimated className='h-8 hidden lg:block' />
                 <LogoMyli className='h-8 block lg:hidden' />
               </Link>
